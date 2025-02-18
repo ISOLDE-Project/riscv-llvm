dee0bb79c8ad17c0c6a9285a952212c2daa61544
daro <daro@tsr>
2025-02-18 06:36:43 +0200

    updated quad-word tests

--- llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
+++ llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
@@ -2060,35 +2060,26 @@ void RISCVDAGToDAGISel::Select(SDNode *Node) {
   }
   case ISD::BUILD_VECTOR: {
     SDLoc DL(Node);
-    MachineFunction &MF = CurDAG->getMachineFunction();
+    //MachineFunction &MF = CurDAG->getMachineFunction();
     auto EltVT = Node->getValueType(0).getVectorElementType();
     llvm::SmallVector<SDValue> Ops;
     for (unsigned int I = 0; I < Node->getNumOperands(); ++I) {
-      //if (I < 4) {
         uint64_t Const1 = 0;
         if (isa<ConstantSDNode>(Node->getOperand(I))) {
           Const1 = Node->getConstantOperandVal(I);
           SDValue Imm1 = CurDAG->getTargetConstant(Const1, DL, EltVT);
           Ops.push_back(Imm1);          
-      //} else {
-       // break;
       }
       }
     //}
     MachineSDNode* loadVector=nullptr;
     unsigned int vecSize = Ops.size();
-    if(2 == vecSize){
-         SDValue imm1 = CurDAG->getTargetConstant(0, DL, EltVT);
-         Ops.push_back(imm1);
-         loadVector = CurDAG->getMachineNode(RISCV::LD3XWORD, DL, MVT::v4i32, Ops);
-         printf("vectorSize=!!!\n");
-    }
+    if(2 == vecSize)
+         loadVector = CurDAG->getMachineNode(RISCV::LD2XWORD, DL, MVT::v4i32, Ops);
     else if(3 == vecSize)
          loadVector = CurDAG->getMachineNode(RISCV::LD3XWORD, DL, MVT::v4i32, Ops);
     else if(4 == vecSize) 
          loadVector = CurDAG->getMachineNode(RISCV::LD4XWORD, DL, MVT::v4i32, Ops);
-    else 
-      printf("unsuported vector size:%d",vecSize);
     if(loadVector)
         ReplaceNode(Node, loadVector);
     return;
--- llvm/test/CodeGen/ISOLDE/test_q2.ll
+++ llvm/test/CodeGen/ISOLDE/test_q2.ll
@@ -7,15 +7,15 @@
 ; RUN: llc -mtriple=riscv32 -mattr=+v -target-abi=ilp32d -filetype=asm < %s | FileCheck %s
 
 
-;build/bin/llc  -mtriple=riscv32 -mattr=+v -target-abi=ilp32d -filetype=asm -o - llvm/test/CodeGen/ISOLDE/test_q.ll
-declare   <3 x i32>  @foo( <3 x i32>  )  
-define <2 x i32> @test(){
-  %input_shape_0 = insertelement <2 x i32> undef, i32 1, i32 0
-  %res = insertelement <2 x i32> %input_shape_0, i32 28, i32 1
-;  %res = call <3 x i32>  @foo( <3 x i32>  %input_shape )
-  ret <2 x i32> %res  
+declare   <4 x i32>  @foo( <4 x i32>  )  
+define <4 x i32> @test(){
+  %input_shape_0 = insertelement <4 x i32> undef,          i32 1,  i32 0
+  %input_shape   = insertelement <4 x i32> %input_shape_0, i32 28, i32 1
+  %res = call <4 x i32>  @foo( <4 x i32>  %input_shape )
+  ret <4 x i32> %res  
 }
 
 
-;CHECK:  ld3xi32 Q10, 1, 28, 28
+;CHECK:             ld2xi32 Q10, 1, 28
+;CHECK-NEXT:        call    foo
 
